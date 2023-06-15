357ed471ca7fa3c50a2ab8b2ea5527479cb44f8c
LarryLuoO <LarryLuo1@users.noreply.github.com>
2023-06-15 17:19:31 +0800

    💫 add project file

---
+++ README.md
@@ -0,0 +1,28 @@
+# README
+
+## Project Configuration
+### 1. Disable sourceMap
+
+![](media/16868197786831/16868199035770.jpg)
+
+### 2. Set iOS camera and microphone permissions
+
+![](media/16868197786831/16868199670979.jpg)
+
+### 3. Set Android camera and microphone permissions
+
+![](media/16868197786831/16868200195.jpg)
+
+## Implement Video Call
+
+### 1. Add a `div` module to the page where Video Call needs to be displayed.
+![](media/16868197786831/16868201149132.jpg)
+
+### 2. Add permission request
+Before initiating a call, we need to request camera and microphone permissions.
+![](media/16868197786831/16868202373722.jpg)
+
+### 3. Add start call method
+![](media/16868197786831/16868202027197.jpg)
+
+> For detailed usage instructions, please refer to the [official documentation](https://docs.zegocloud.com/article/14933).
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+
+declare var cordova: any;
 
 @Component({
   selector: 'app-home',
@@ -9,4 +12,71 @@ export class HomePage {
 
   constructor() {}
 
+  requestCameraPermissions() {
+    cordova.plugins.permissions.requestPermissions(
+      [
+        cordova.plugins.permissions.CAMERA
+      ],
+      function(status: { hasPermission: any; }) {
+        if (status.hasPermission) {
+          console.log('Permission granted');
+        } else {
+          console.log('Permission denied');
+        }
+      },
+      function() {
+        console.log('Permission request error');
+      }
+    );
+  }
+
+  requestMicPermissions() {
+    cordova.plugins.permissions.requestPermissions(
+      [
+        cordova.plugins.permissions.RECORD_AUDIO
+      ],
+      function(status: { hasPermission: any; }) {
+        if (status.hasPermission) {
+          console.log('Permission granted');
+        } else {
+          console.log('Permission denied');
+        }
+      },
+      function() {
+        console.log('Permission request error');
+      }
+    );
+  }
+
+  startRTC() {
+
+    const myText: string = "Hello World";
+    console.log(myText);
+    console.log('------',navigator.userAgent)
+    // Set the required parameters.
+    const roomID: string = '123456';
+    const userID: string = Math.floor(Math.random() * 10000) + "";
+    const userName: string = "userName" + userID;
+    const appID: number = Your AppID;
+    const serverSecret: string = "Your Server Secret";
+
+    // Generate a Kit Token using the ZegoUIKitPrebuilt method.
+    const kitToken: string = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, userID, userName);
+
+    // Create an instance of ZegoUIKitPrebuilt and join the room.
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    zp.joinRoom({
+      //@ts-ignore
+      container: document.querySelector("#root"),
+      showPreJoinView: false,
+      showLeavingView: false,
+      videoCodec:"VP8",
+      sharedLinks: [{
+        url: window.location.protocol + '//' + window.location.host + window.location.pathname + '?roomID=' + roomID,
+      }],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoScenario.OneONoneCall].
+      }
+    });
+  }
 }
